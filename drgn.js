eb85c1fe8544cf656044a11c2405b04abc3ea705
gapehorm <165452367+gapehorm@users.noreply.github.com>
2024-04-11 16:31:48 +0530

    Create drgn.js

---
+++ drgn.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+function connectWallet() {
+    // Add your logic to redirect the user to the connect wallet page
+    // For example:
+    window.location.href = "https://example.com/connect-wallet";
+}
